586f428d6187c9db83621ffa26b3f88c0d6d08a4
LSH <tkdgns1284@gmail.com>
2020-03-26 20:36:38 +0900

    GameEnding added,  Gagoyle added.

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration=1f;
+    public float displayImageDuration = 1f;
+
+    public CanvasGroup fadeWonImageCanvasGroup;
+    public CanvasGroup fadeCaughtImageCanvasGroup;
+
+    public void CaughtEnd() {
+        StartCoroutine(EndGameAsync(fadeCaughtImageCanvasGroup, true));
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag=="Player") {
+            StartCoroutine(EndGameAsync(fadeWonImageCanvasGroup,false));
+        }
+    }
+
+    private IEnumerator EndGameAsync(CanvasGroup fadeImage,bool restart) {
+        float t = 0f;
+        while (true) {
+            t += Time.deltaTime;
+            var alpha = t / fadeDuration;
+            fadeImage.alpha = alpha;
+            if (Mathf.Approximately(alpha,1f) || alpha > 1f ) {
+                break;
+            }
+            yield return null;
+        }
+        fadeImage.alpha = 1f;
+        yield return new WaitForSeconds(displayImageDuration);
+
+        if (restart) {
+            SceneManager.LoadScene(0);
+        }else {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+
+    }
+}
---
+++ Assets/Scripts/Observer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observer : MonoBehaviour
+{
+
+    private bool isPlayerInRange;
+    private GameObject player;
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player") {
+            isPlayerInRange = true;
+            player = other.gameObject;
+        }
+    }
+    private void OnTriggerExit(Collider other) {
+        if(other.tag == "Player") {
+            isPlayerInRange = false;
+            player = null;
+        }
+    }
+
+    private void Update() {
+        if (isPlayerInRange) {
+            Vector3 direction = (player.transform.position + Vector3.up) - transform.position;
+
+            Ray ray = new Ray(transform.position,direction);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray,out hit)) {
+                if (hit.collider.tag=="Player") {
+                    FindObjectOfType<GameEnding>().CaughtEnd();
+                }
+            }
+        }
+    }
+}
