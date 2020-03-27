f1aea0eb61f5ddee72d9c53a6d644f3b6debd466
LSH <tkdgns1284@gmail.com>
2020-03-27 22:01:19 +0900

    ghost added

---
+++ Assets/Scripts/WaypointPatrol.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WaypointPatrol : MonoBehaviour
+{
+    public List<Transform> waypoints = new List<Transform>();
+
+    private NavMeshAgent mNavMeshAgent => GetComponent<NavMeshAgent>();
+    public int curWaypointIndex;
+
+    void Start()
+    {
+        mNavMeshAgent.SetDestination(waypoints[0].position);
+    }
+    
+    void Update()
+    {
+        if (mNavMeshAgent.remainingDistance< mNavMeshAgent.stoppingDistance) {
+            curWaypointIndex = (curWaypointIndex + 1) % waypoints.Count;
+            mNavMeshAgent.SetDestination(waypoints[curWaypointIndex].position);
+        }
+    }
+}
